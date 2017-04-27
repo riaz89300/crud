05e15d4661ef91afaf94ed0889525262fd284ee8
yasir <yasirali.chippa@gmail.com>
2017-04-27 12:39:20 +0500

    Practise

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base  
+   validates :image, presence: true
+
+  has_attached_file :image, default_url: "/image/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+end  
